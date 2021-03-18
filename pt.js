fb95063832df40c7db3f1e3dfda30337f1b264da
zjadaczfantow <darek.rejdych32@gmail.com>
2021-03-18 15:06:32 +0100

    Oddzielenie CSS i JS do osobnych plikow. (internal -> external)

---
+++ pt.js
@@ -0,0 +1,73 @@
+
+function openK1()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k1.html";
+}
+function openK2()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k2.html";
+}
+function openK3()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k3.html";
+}
+function openK4()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k4.html";
+}
+function openK5()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k5.html";
+}
+function openK6()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k6.html";
+}
+function openK7()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k7.html";
+}
+function openK8()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k8.html";
+}
+function openK9()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k9.html";
+}
+function openK10()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k10.html";
+}
+function openK11()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k11.html";
+}
+function openK12()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "k12.html";
+}
